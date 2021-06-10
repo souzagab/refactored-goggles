cdee1fafb284db41f18fec169738d7bc99cbccbf
Gabriel S <thesouzagab@gmail.com>
2021-06-10 20:52:13 -0300

    super secure auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authorize
+
+  private
+
+  def authorize
+    return if request.headers["Authorization"].include? ENV.fetch "API_SECRET" # super secure
+
+    head :unauthorized
+  end
 end
