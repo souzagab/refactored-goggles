78001785bbe2af8810362befdc8effe1faac1a96
Gabriel S <thesouzagab@gmail.com>
2021-06-10 20:52:03 -0300

    scaffold all the way

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+  {
+    name: 'Produto 1',
+    description: 'Produto 1',
+    price: 10.0
+  },
+  {
+    name: 'Produto 2',
+    description: 'Produto 2',
+    price: 10.0
+  },
+])
